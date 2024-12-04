980d4fb52f9edd3a9a88f67f6f9ad3049c8717a5
yush-nh <yushin.nihei@luxiar.com>
2024-12-04 11:28:19 +0900

    ADDED access token generate function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+	resources :access_tokens, only: %i[create destroy]
 
 	get '/users/:email' => 'users#show', :constraints => { :email => /.+@.+\..*/ }, as: 'user'
 	post '/configs/:id', to: 'configs#update'
