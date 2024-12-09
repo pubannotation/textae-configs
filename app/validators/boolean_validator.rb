5b553e5ae279377b093ff4343c7e46feea2a00d2
yush-nh <yushin.nihei@luxiar.com>
2024-12-09 17:52:29 +0900

    ADDED boolean validation to is_public

---
+++ app/validators/boolean_validator.rb
@@ -0,0 +1,6 @@
+class BooleanValidator < ActiveModel::EachValidator
+  def validate_each(record, attr, _value)
+    before_value = record.send("#{attr}_before_type_cast")
+    record.errors.add(attr, "must be true or false.") unless %w[true false 1 0].include?(before_value.to_s.downcase)
+  end
+end
