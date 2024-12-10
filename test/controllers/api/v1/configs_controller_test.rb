f48063c087230bc9e1b89dbe3128d823cab429f2
yush-nh <yushin.nihei@luxiar.com>
2024-12-10 19:17:01 +0900

    FIXED api test to support client credential authentication

--- test/controllers/api/v1/configs_controller_test.rb
+++ test/controllers/api/v1/configs_controller_test.rb
@@ -4,8 +4,9 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   setup do
-    @user = users(:one)
-    sign_in @user
+    user = users(:one)
+    user.create_access_token!
+    @access_token = user.access_token.token
     @config = configs(:two)
   end
 
@@ -13,7 +14,8 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
   test "POST_should_create_config" do
     assert_difference("Config.count", 1) do
       post "/api/v1/configs/test-config", params: '{}',
-                                          headers: { 'Content-Type': 'application/json' }
+                                          headers: { 'Content-Type' => 'application/json',
+                                                     'Authorization' => "Bearer #{@access_token}" }
     end
 
     assert_response 201
@@ -22,14 +24,16 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
 
   test "POST_should_save_body_without_format" do
     post "/api/v1/configs/test-config", params: '{ "attribute types": "value" }',
-                                        headers: { 'Content-Type': 'application/json' }
+                                        headers: { 'Content-Type' => 'application/json',
+                                                   'Authorization' => "Bearer #{@access_token}" }
 
     assert_equal JSON.generate({ "attribute types": "value" }), Config.last.body
   end
 
   test "POST_should_save_attributes_with_query_parameter" do
     post "/api/v1/configs/test-config\?description\=abc\&is_public\=true", params: '{}',
-                                                                           headers: { 'Content-Type': 'application/json' }
+                                                                           headers: { 'Content-Type' => 'application/json',
+                                                                                      'Authorization' => "Bearer #{@access_token}" }
 
     assert_equal "abc", Config.last.description
     assert_equal true, Config.last.is_public
@@ -53,7 +57,8 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
   # Test PUT/PATCH api/v1/configs/name
   test "PUT_should_update_config" do
     put "/api/v1/configs/#{@config.name}", params: '{ "attribute types": "new_value" }',
-                                           headers: { 'Content-Type': 'application/json' }
+                                           headers: { 'Content-Type' => 'application/json',
+                                                      'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 200
     assert_equal "Config #{@config.name} was successfully updated.", JSON.parse(response.body)['message']
@@ -62,7 +67,7 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
 
   test "UPDATE_should_update_attributes_with_query_parameter" do
 
-    put "/api/v1/configs/#{@config.name}\?description\=def\&is_public\=true", headers: { 'Content-Type': 'application/json' }
+    put "/api/v1/configs/#{@config.name}\?description\=def\&is_public\=true", headers: { 'Authorization' => "Bearer #{@access_token}" }
 
     assert_equal "def", Config.friendly.find(@config.name).description
     assert_equal true , Config.friendly.find(@config.name).is_public
@@ -71,7 +76,7 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
   # Test DELETE api/v1/configs/name
   test "DELETE_should_delete_config" do
     assert_difference("Config.count", -1) do
-      delete "/api/v1/configs/#{@config.name}"
+      delete "/api/v1/configs/#{@config.name}", headers: { 'Authorization' => "Bearer #{@access_token}" }
     end
 
     assert_response 200
@@ -81,7 +86,8 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
   # Test exceptions
   test "should_return_400_when_request_json_is_invalid_format" do
     post "/api/v1/configs/invalid-config", params: 'invalid json',
-                                           headers: { 'Content-Type': 'application/json' }
+                                           headers: { 'Content-Type' => 'application/json',
+                                                      'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 400
     assert_equal "Invalid JSON format.", JSON.parse(response.body)['error']
@@ -94,25 +100,28 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
     assert_response 404
     assert_equal expected_message, JSON.parse(response.body)['error']
 
-    put "/api/v1/configs/non-existent-config", params: '{}', headers: { 'Content-Type': 'application/json' }
+    put "/api/v1/configs/non-existent-config", params: '{}', headers: { 'Content-Type': 'application/json',
+                                                                        'Authorization' => "Bearer #{@access_token}" }
     assert_response 404
     assert_equal expected_message, JSON.parse(response.body)['error']
 
-    delete "/api/v1/configs/non-existent-config"
+    delete "/api/v1/configs/non-existent-config", headers: { 'Authorization' => "Bearer #{@access_token}" }
     assert_response 404
     assert_equal expected_message, JSON.parse(response.body)['error']
   end
 
   test "should_return_409_when_config_is_already_exists" do
     post "/api/v1/configs/#{@config.name}", params: '{ "attribute types": "value" }',
-                                            headers: { 'Content-Type': 'application/json' }
+                                            headers: { 'Content-Type': 'application/json',
+                                                       'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 409
     assert_equal "#{@config.name} has already been taken.", JSON.parse(response.body)['error']
   end
 
   test "should_return_422_when_request_body_is_not_present" do
-    post "/api/v1/configs/test-config"
+    post "/api/v1/configs/test-config", headers: { 'Content-Type': 'application/json',
+                                                   'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 422
     assert_equal "Validation failed: Body can't be blank", JSON.parse(response.body)['error']
@@ -120,14 +129,16 @@ class Api::V1::ConfigsControllerTest < ActionDispatch::IntegrationTest
 
   test "should_return_422_when_config_name_is_invalid" do
     post "/api/v1/configs/abc@def", params: '{}',
-                                    headers: { 'Content-Type': 'application/json' }
+                                    headers: { 'Content-Type': 'application/json',
+                                               'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 422
   end
 
   test "should_return_422_when_is_public_is_not_boolean" do
     post "/api/v1/configs/test-config\?is_public\=abc", params: '{}',
-                                                        headers: { 'Content-Type': 'application/json' }
+                                                        headers: { 'Content-Type': 'application/json',
+                                                                   'Authorization' => "Bearer #{@access_token}" }
 
     assert_response 422
   end
