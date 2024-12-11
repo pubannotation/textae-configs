c27ba3b8fc42902018e9d100549a62cb2ef39ba7
yush-nh <yushin.nihei@luxiar.com>
2024-12-11 13:26:13 +0900

    ADDED BooleanValidator explanation

--- app/validators/boolean_validator.rb
+++ app/validators/boolean_validator.rb
@@ -1,3 +1,6 @@
+# BooleanValidator prevents Rails type conversion from storing the true/false value as an unexpected value
+# when it is received as a string.
+
 class BooleanValidator < ActiveModel::EachValidator
   def validate_each(record, attr, _value)
     raw_value = record.public_send("#{attr}_before_type_cast")
