848cef88db89b877eb118effa5e2cb9789a75e12
yush-nh <yushin.nihei@luxiar.com>
2024-12-11 14:57:40 +0900

    CHANGED indentation to space

--- app/helpers/configs_helper.rb
+++ app/helpers/configs_helper.rb
@@ -1,12 +1,12 @@
 module ConfigsHelper
 
-	def badge_is_public(config)
-		badge, btitle = if config.is_public
-		else
-			['<i class="fa fa-eye-slash" aria-hidden="true"></i>', 'Hidden']
-		end
+  def badge_is_public(config)
+    badge, btitle = if config.is_public
+    else
+      ['<i class="fa fa-eye-slash" aria-hidden="true"></i>', 'Hidden']
+    end
 
-		badge.present? ? "<span class='badge' title='#{btitle}'>#{badge}</span>".html_safe : ""
-	end
+    badge.present? ? "<span class='badge' title='#{btitle}'>#{badge}</span>".html_safe : ""
+  end
 
 end
