8dbcba23b70c2c62234b8f49ed4b1945d0082350
yush-nh <yushin.nihei@luxiar.com>
2024-12-11 09:53:04 +0900

    FIXED exception handling method name and position

--- app/controllers/api/v1/configs_controller.rb
+++ app/controllers/api/v1/configs_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::ConfigsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_access_token, only: %i[create update destroy]
 
-  rescue_from StandardError, with: :handle_standard_error
-  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-  rescue_from ActiveRecord::RecordNotUnique, with: :record_already_exists
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :parse_error
+  rescue_from StandardError, with: :render_standard_error
+  rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_error
+  rescue_from ActiveRecord::RecordNotUnique, with: :render_record_already_exists_error
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_error
+  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :render_parse_error
 
   # GET api/v1/configs/name
   def show
@@ -64,11 +64,6 @@ class Api::V1::ConfigsController < ApplicationController
     end
   end
 
-  def render_token_invalid_error
-    render json: { error: "The access token is missing or invalid." }, status: :unauthorized
-    response.headers['WWW-Authenticate'] = 'Bearer'
-  end
-
   def current_config
     current_user.configs.friendly.find(params[:name])
   end
@@ -83,23 +78,28 @@ class Api::V1::ConfigsController < ApplicationController
     config
   end
 
-  def handle_standard_error(e)
+  def render_standard_error(e)
     render json: { error: e.message }, status: :internal_server_error
   end
 
-  def record_invalid(e)
+  def render_token_invalid_error
+    render json: { error: "The access token is missing or invalid." }, status: :unauthorized
+    response.headers['WWW-Authenticate'] = 'Bearer'
+  end
+
+  def render_record_invalid_error(e)
     render json: { error: e.message }, status: :unprocessable_entity
   end
 
-  def record_already_exists
+  def render_record_already_exists_error
     render json: { error: "#{params[:name]} has already been taken." }, status: :conflict
   end
 
-  def record_not_found
+  def render_record_not_found_error
     render json: { error: "Could not find the config #{params[:name]}" }, status: :not_found
   end
 
-  def parse_error
+  def render_parse_error
     render json: { error: 'Invalid JSON format.' }, status: :bad_request
   end
 
