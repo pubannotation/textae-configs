b7d600948886d9959257c9882c4616cae8bb9893
yush-nh <yushin.nihei@luxiar.com>
2024-12-11 13:19:34 +0900

    REVISED variable name

--- app/validators/boolean_validator.rb
+++ app/validators/boolean_validator.rb
@@ -1,6 +1,6 @@
 class BooleanValidator < ActiveModel::EachValidator
   def validate_each(record, attr, _value)
-    before_value = record.public_send("#{attr}_before_type_cast")
-    record.errors.add(attr, "must be true or false.") unless %w[true false 1 0].include?(before_value.to_s.downcase)
+    raw_value = record.public_send("#{attr}_before_type_cast")
+    record.errors.add(attr, "must be true or false.") unless %w[true false 1 0].include?(raw_value.to_s.downcase)
   end
 end
