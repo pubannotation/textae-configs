c6c14fd7c3154f80fc0596c7062dd6c10e83f47e
yush-nh <yushin.nihei@luxiar.com>
2024-12-12 11:22:57 +0900

    REVISED BooleanValidator explanation

--- app/validators/boolean_validator.rb
+++ app/validators/boolean_validator.rb
@@ -1,5 +1,12 @@
-# BooleanValidator prevents unexpected Rails type conversion.
-# For example, it prevents the value like ‘foo’ or ‘bar’ from being saved as true.
+# In order to clarify the API specification, I would like to restrict the input values
+# to '1', 'true', '0', and 'false' for parameters that receive Boolean values.
+
+# However, when using Rails built-in validator, values like 'foo' and 'bar' are also accepted as true.
+# This is because Rails built-in validator checks the value after Rails type casting process.
+# In Rails' type casting, strings that are not false are converted to true.
+
+# The BooleanValidator checks the value before type casting, and prevents values like 'foo' and 'bar' from being saved as true.
+# Example of use: `validates :is_public, boolean: true`
 
 class BooleanValidator < ActiveModel::EachValidator
   VALID_VALUES = [
