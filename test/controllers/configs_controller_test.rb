0a9ba48e99ae744a7a21cfba76e9641b341f16f7
yush-nh <yushin.nihei@luxiar.com>
2025-03-25 17:12:22 +0900

    Fix old failing tests

--- test/controllers/configs_controller_test.rb
+++ test/controllers/configs_controller_test.rb
@@ -1,14 +1,17 @@
 require 'test_helper'
 
 class ConfigsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
   setup do
     @config = configs(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:configs)
   end
 
   test "should get new" do
@@ -18,30 +21,48 @@ class ConfigsControllerTest < ActionController::TestCase
 
   test "should create config" do
     assert_difference('Config.count') do
-      post :create, config: { body: @config.body, description: @config.description, is_public: @config.is_public, name: @config.name }
+      post :create, params: {
+                      config: {
+                        body: @config.body,
+                        description: @config.description,
+                        is_public: @config.is_public,
+                        name: "new-config"
+                      }
+                    }
     end
 
-    assert_redirected_to config_path(assigns(:config))
+    created_config = Config.find_by(name: "new-config")
+    assert_redirected_to config_path(created_config)
   end
 
   test "should show config" do
-    get :show, id: @config
+    get :show, params: { id: @config.id }
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @config
+    get :edit, params: { id: @config.id }
     assert_response :success
   end
 
   test "should update config" do
-    patch :update, id: @config, config: { body: @config.body, description: @config.description, is_public: @config.is_public, name: @config.name }
-    assert_redirected_to config_path(assigns(:config))
+    patch :update, params: {
+                    id: @config.id,
+                    config: {
+                      body: @config.body,
+                      description: @config.description,
+                      is_public: @config.is_public,
+                      name: @config.name
+                    }
+                  }
+
+    updated_config = Config.find(@config.id)
+    assert_redirected_to config_path(updated_config)
   end
 
   test "should destroy config" do
     assert_difference('Config.count', -1) do
-      delete :destroy, id: @config
+      delete :destroy, params: { id: @config.id }
     end
 
     assert_redirected_to configs_path
