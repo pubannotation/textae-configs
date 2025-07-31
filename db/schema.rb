28fec2e1224389a827bbe9b25be467b55d9c2eb4
jdkim <jindong.kim@gmail.com>
2025-07-31 22:39:50 +0900

    FIXED Oauth2 authentication with Google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_04_004344) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_04_004344) do
   create_table "access_tokens", force: :cascade do |t|
     t.string "token", null: false
     t.integer "user_id", null: false
@@ -24,8 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_04_004344) do
     t.text "description"
     t.text "body"
     t.boolean "is_public", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.index ["name"], name: "index_configs_on_name", unique: true
     t.index ["user_id"], name: "index_configs_on_user_id"
@@ -35,20 +35,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_04_004344) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "provider"
     t.string "uid"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
